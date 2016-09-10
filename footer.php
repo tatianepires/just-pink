8ab0b730963b1feffe48461ef2c6b71fd50977dd
Tatiane Pires <tatiane@tatianepires.net>
2016-09-10 18:53:38 -0300

    Changed author URL to tpires.me

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 	</div><!-- #main .wrapper -->
 	<footer id="colophon" role="contentinfo">
 		<div class="site-info site">
-			<?php printf( __( 'Proudly powered by %s', 'justpink'), ''); ?> <a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentytwelve' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentytwelve' ); ?>">WordPress</a>. Just Pink by tpires.net.
+			<?php printf( __( 'Proudly powered by %s', 'justpink'), ''); ?> <a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentytwelve' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentytwelve' ); ?>">WordPress</a>. Just Pink by <a href="http://tpires.me/">tpires.me</a>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
