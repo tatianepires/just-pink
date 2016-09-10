664831bffa9258e05c1ea04f1261f92780d77065
Tatiane Pires <tatiane@tatianepires.net>
2016-09-10 18:50:13 -0300

    Removed menu toggle btn

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 <!--[if IE 8]>
 <html class="ie ie8" <?php language_attributes(); ?>>
 <![endif]-->
-<!--[if !(IE 7) | !(IE 8)  ]><!-->
+<!--[if !(IE 7) & !(IE 8)]><!-->
 <html <?php language_attributes(); ?>>
 <!--<![endif]-->
 <head>
@@ -21,6 +21,7 @@
 <![endif]-->
 <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed">
 	<header id="masthead" class="site-header" role="banner">
@@ -41,7 +42,6 @@
 	
 	<nav id="site-navigation" class="main-navigation" role="navigation">
 		<div class="site">
-			<h3 class="menu-toggle"><?php _e( 'Menu', 'justpink' ); ?></h3>
 			<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'justpink' ); ?>"><?php _e( 'Skip to content', 'justpink' ); ?></a>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
 			
